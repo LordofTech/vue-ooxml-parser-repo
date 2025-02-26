e75c6ecb0c275849e208c5b6838c8aee6b2824dc
LordofTech <chukwurahnnamdi81@gmail.com>
2025-02-26 07:16:38 +0100

    Add files via upload

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>OOXML Document Parser</h1>
+    <UploadFile />
+  </div>
+</template>
+
+<script>
+import UploadFile from './components/UploadFile.vue';
+
+export default {
+  components: {
+    UploadFile,
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  font-family: Arial, sans-serif;
+  margin: 20px;
+}
+</style>
